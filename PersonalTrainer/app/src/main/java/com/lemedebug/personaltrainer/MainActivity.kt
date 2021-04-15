e4f41331f1fba20fdf2bb51bff61c7f341070a49
Mansimran Singh <42754853+Mansimran-Singh@users.noreply.github.com>
2021-04-15 16:35:49 -0400

    Added WorkoutActivity, BMIActivity | Fixed Main UI | Added ActionBar Feature to Activites

--- PersonalTrainer/app/src/main/java/com/lemedebug/personaltrainer/MainActivity.kt
+++ PersonalTrainer/app/src/main/java/com/lemedebug/personaltrainer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lemedebug.personaltrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,55 +18,23 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private val BASE_URL = "https://wger.de/api/v2/exerciseinfo/"
-    private val TAG = "MAIN_ACTIVITY"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exerciseList = ArrayList<Exercise>()
 
-        val adapter = ExerciseAdapter(exerciseList)
+        llBMI.setOnClickListener {
+            // Launching the BMI Activity
+            val intent = Intent(this, BMIActivity::class.java)
+            startActivity(intent)
+        }
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_exercise_list)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        llStart.setOnClickListener {
+            //Launching the WorkoutActivity
+            val intent = Intent(this, WorkoutActivity::class.java)
+            startActivity(intent)
+        }
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val randomExerciseAPI = retrofit.create(ExerciseService::class.java)
-        randomExerciseAPI.getExerciseList(2, 200).enqueue(object : Callback<ExerciseData> {
-            override fun onResponse(call: Call<ExerciseData>, response: Response<ExerciseData>) {
-                Log.d(TAG, "OnResponse: $response")
-                val body = response.body()
-                if (body == null) {
-                    Log.d(TAG, "Invalid Response Found")
-                    return
-                }
-
-                Log.d(TAG, body.results[0].id)
-                Log.d(TAG, body.results[0].name)
-                Log.d(TAG, body.results[0].description)
-                Log.d(TAG, body.results[0].category.name)
-                Log.d(TAG, body.results[0].comments.toString())
-                Log.d(TAG, body.results[0].equipment.toString())
-                Log.d(TAG, body.results[0].images.toString())
-                Log.d(TAG, body.results[0].muscles.toString())
-                Log.d(TAG, body.results[0].muscles_secondary.toString())
-
-                exerciseList.addAll(body.results)
-                adapter.notifyDataSetChanged()
-            }
-
-            override fun onFailure(call: Call<ExerciseData>, t: Throwable) {
-                Log.d(TAG, "OnFailure: $t")
-            }
-
-        })
     }
 
 
